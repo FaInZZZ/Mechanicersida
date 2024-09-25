9021ba9bec298f55b69ca729407a63574a040904
Kevin Brandt <keppe06@gmail.com>
2024-09-25 10:35:27 +0300

    data

--- 2024_powerol.sql
+++ 2024_powerol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2024 at 12:22 PM
+-- Generation Time: Sep 25, 2024 at 09:34 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,13 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `table_cars` (
-  `c_id` int(11) NOT NULL,
-  `c_brand` varchar(255) NOT NULL,
-  `c_model` varchar(255) NOT NULL,
-  `c_license` varchar(255) NOT NULL,
-  `c_problem` varchar(512) NOT NULL,
-  `c_description` varchar(512) NOT NULL,
-  `c_product` varchar(255) NOT NULL
+  `id_cars` int(11) NOT NULL,
+  `car_brand` varchar(255) NOT NULL,
+  `car_model` varchar(255) NOT NULL,
+  `car_register` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -44,11 +41,11 @@ CREATE TABLE `table_cars` (
 --
 
 CREATE TABLE `table_customer` (
-  `cust_id` int(11) NOT NULL,
+  `id_cust` int(11) NOT NULL,
   `cust_fname` varchar(255) NOT NULL,
   `cust_lname` varchar(255) NOT NULL,
-  `cust_epost` varchar(255) NOT NULL,
   `cust_tel` varchar(255) NOT NULL,
+  `cust_epost` varchar(255) NOT NULL,
   `cust_adress` varchar(255) NOT NULL,
   `cust_postnummer` varchar(255) NOT NULL,
   `cust_ort` varchar(255) NOT NULL
@@ -58,8 +55,50 @@ CREATE TABLE `table_customer` (
 -- Dumping data for table `table_customer`
 --
 
-INSERT INTO `table_customer` (`cust_id`, `cust_fname`, `cust_lname`, `cust_epost`, `cust_tel`, `cust_adress`, `cust_postnummer`, `cust_ort`) VALUES
-(1, 'eee', 'regre', 'greg@ef.fi', 'gregr', 'ergr', 'fwef', 'fwefwefe');
+INSERT INTO `table_customer` (`id_cust`, `cust_fname`, `cust_lname`, `cust_tel`, `cust_epost`, `cust_adress`, `cust_postnummer`, `cust_ort`) VALUES
+(3, 'dwada', 'dada', 'dada', 'dada@dawd', 'dadad', 'adad', 'adad'),
+(4, 'Kevin', 'Bränä', '0449574723', 'kevinsepost@gmail.com', 'hemma', '10640', 'sverige'),
+(5, 'Kevin', 'Bränä', '0449574723', 'kevinsepost@gmail.com', 'hemma', '10640', 'sverige'),
+(6, 'ok', 'hej', '3', '2@wdwada', '21', '21', '211');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_parts`
+--
+
+CREATE TABLE `table_parts` (
+  `id_produkt` int(11) NOT NULL,
+  `produkt_namn` varchar(255) NOT NULL,
+  `produkt_pris` decimal(7,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_produkter_i_projekt`
+--
+
+CREATE TABLE `table_produkter_i_projekt` (
+  `projekt_id_fk` int(11) NOT NULL,
+  `produkt_id_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_projekt`
+--
+
+CREATE TABLE `table_projekt` (
+  `id_projekt` int(11) NOT NULL,
+  `pt_felbeskrivning` varchar(520) NOT NULL,
+  `pt_arbetsbeskrivning` varchar(520) NOT NULL,
+  `pt_status_fk` int(11) NOT NULL,
+  `cars_fk` int(11) NOT NULL,
+  `customer_fk` int(11) NOT NULL,
+  `created_by_user_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -69,8 +108,43 @@ INSERT INTO `table_customer` (`cust_id`, `cust_fname`, `cust_lname`, `cust_epost
 
 CREATE TABLE `table_roles` (
   `r_id` int(11) NOT NULL,
-  `r_name` int(11) NOT NULL,
+  `r_name` varchar(255) NOT NULL,
   `r_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `table_roles`
+--
+
+INSERT INTO `table_roles` (`r_id`, `r_name`, `r_level`) VALUES
+(1, 'Mechanicer', 10),
+(2, 'Fakturerare', 50),
+(3, 'Administrator', 500),
+(4, 'Chefen', 300);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_status`
+--
+
+CREATE TABLE `table_status` (
+  `id_status` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_timmar`
+--
+
+CREATE TABLE `table_timmar` (
+  `id_timmar` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `amount` decimal(10,0) NOT NULL,
+  `pt_fk` int(11) NOT NULL,
+  `user_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -81,20 +155,12 @@ CREATE TABLE `table_roles` (
 
 CREATE TABLE `table_users` (
   `u_id` int(11) NOT NULL,
-  `u_name` varchar(256) NOT NULL,
-  `u_password` varchar(256) NOT NULL,
-  `u_email` varchar(256) NOT NULL,
+  `u_name` varchar(255) NOT NULL,
+  `u_password` varchar(255) NOT NULL,
+  `u_email` varchar(255) NOT NULL,
   `u_role_fk` int(11) NOT NULL,
-  `u_status` int(11) NOT NULL
+  `u_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `table_users`
---
-
-INSERT INTO `table_users` (`u_id`, `u_name`, `u_password`, `u_email`, `u_role_fk`, `u_status`) VALUES
-(1, 'kevinbrandt', '$2y$10$T6cDcLs3NKqFKWQO9m4ZFujQH9lKAEhefbiRPuEuum1Ojq5i0df.K', 'hejpadig@gmail.com', 1, 1),
-(2, 'qarlsson', '$2y$10$VD0STfjYgK6XStBCPuPvPul3NzlIPfnFaR63zjIXjZYBmOCXOkC3y', 'kebab@gmail.com', 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -104,13 +170,36 @@ INSERT INTO `table_users` (`u_id`, `u_name`, `u_password`, `u_email`, `u_role_fk
 -- Indexes for table `table_cars`
 --
 ALTER TABLE `table_cars`
-  ADD PRIMARY KEY (`c_id`);
+  ADD PRIMARY KEY (`id_cars`);
 
 --
 -- Indexes for table `table_customer`
 --
 ALTER TABLE `table_customer`
-  ADD PRIMARY KEY (`cust_id`);
+  ADD PRIMARY KEY (`id_cust`);
+
+--
+-- Indexes for table `table_parts`
+--
+ALTER TABLE `table_parts`
+  ADD PRIMARY KEY (`id_produkt`);
+
+--
+-- Indexes for table `table_produkter_i_projekt`
+--
+ALTER TABLE `table_produkter_i_projekt`
+  ADD PRIMARY KEY (`projekt_id_fk`),
+  ADD KEY `produkt_id_fk` (`produkt_id_fk`);
+
+--
+-- Indexes for table `table_projekt`
+--
+ALTER TABLE `table_projekt`
+  ADD PRIMARY KEY (`id_projekt`),
+  ADD KEY `pt_status_fk` (`pt_status_fk`,`cars_fk`,`customer_fk`,`created_by_user_fk`),
+  ADD KEY `fk3` (`created_by_user_fk`),
+  ADD KEY `fk5` (`customer_fk`),
+  ADD KEY `fk4` (`cars_fk`);
 
 --
 -- Indexes for table `table_roles`
@@ -119,10 +208,23 @@ ALTER TABLE `table_roles`
   ADD PRIMARY KEY (`r_id`);
 
 --
+-- Indexes for table `table_status`
+--
+ALTER TABLE `table_status`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- Indexes for table `table_timmar`
+--
+ALTER TABLE `table_timmar`
+  ADD PRIMARY KEY (`id_timmar`);
+
+--
 -- Indexes for table `table_users`
 --
 ALTER TABLE `table_users`
-  ADD PRIMARY KEY (`u_id`);
+  ADD PRIMARY KEY (`u_id`),
+  ADD KEY `u_role_fk` (`u_role_fk`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -132,25 +234,82 @@ ALTER TABLE `table_users`
 -- AUTO_INCREMENT for table `table_cars`
 --
 ALTER TABLE `table_cars`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cars` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `table_customer`
 --
 ALTER TABLE `table_customer`
-  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_cust` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `table_parts`
+--
+ALTER TABLE `table_parts`
+  MODIFY `id_produkt` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_produkter_i_projekt`
+--
+ALTER TABLE `table_produkter_i_projekt`
+  MODIFY `projekt_id_fk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_projekt`
+--
+ALTER TABLE `table_projekt`
+  MODIFY `id_projekt` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `table_roles`
 --
 ALTER TABLE `table_roles`
-  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=402;
+
+--
+-- AUTO_INCREMENT for table `table_status`
+--
+ALTER TABLE `table_status`
+  MODIFY `id_status` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `table_timmar`
+--
+ALTER TABLE `table_timmar`
+  MODIFY `id_timmar` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `table_users`
 --
 ALTER TABLE `table_users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `table_produkter_i_projekt`
+--
+ALTER TABLE `table_produkter_i_projekt`
+  ADD CONSTRAINT `fk133` FOREIGN KEY (`projekt_id_fk`) REFERENCES `table_parts` (`id_produkt`),
+  ADD CONSTRAINT `fk4849453` FOREIGN KEY (`projekt_id_fk`) REFERENCES `table_timmar` (`id_timmar`);
+
+--
+-- Constraints for table `table_projekt`
+--
+ALTER TABLE `table_projekt`
+  ADD CONSTRAINT `fk1` FOREIGN KEY (`pt_status_fk`) REFERENCES `table_status` (`id_status`),
+  ADD CONSTRAINT `fk2` FOREIGN KEY (`id_projekt`) REFERENCES `table_produkter_i_projekt` (`projekt_id_fk`),
+  ADD CONSTRAINT `fk3` FOREIGN KEY (`created_by_user_fk`) REFERENCES `table_users` (`u_id`),
+  ADD CONSTRAINT `fk4` FOREIGN KEY (`cars_fk`) REFERENCES `table_cars` (`id_cars`),
+  ADD CONSTRAINT `fk5` FOREIGN KEY (`customer_fk`) REFERENCES `table_customer` (`id_cust`);
+
+--
+-- Constraints for table `table_users`
+--
+ALTER TABLE `table_users`
+  ADD CONSTRAINT `fk312132` FOREIGN KEY (`u_id`) REFERENCES `table_timmar` (`id_timmar`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
