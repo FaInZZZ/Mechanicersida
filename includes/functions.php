21ee2936d7d68cf0cddbff95bd1222c1df4483e8
Kevin Brandt <keppe06@gmail.com>
2024-09-25 11:30:39 +0300

    A

--- includes/functions.php
+++ includes/functions.php
@@ -25,16 +25,15 @@ function nyprjkt($pdo) {
 
     
 
-    $stmt_inserpjk = $pdo->prepare('INSERT INTO table_projekt (customer_fk, pt_felbeskrivning, pt_arbetsbeskrivning) 
-                                      VALUES (:)');
+    $stmt_inserpjk = $pdo->prepare('INSERT INTO table_projekt (customer_fk, pt_felbeskrivning, pt_arbetsbeskrivning, car_brand, car_model, car_reg ) 
+                                      VALUES (:customer_fk, :pt_felbeskrivning, :pt_arbetsbeskrivning, :car_brand, :car_model, :car_reg)');
 
-    $stmt_inserpjk->bindParam(':cust_fname', $_POST['namn'], PDO::PARAM_STR);
-    $stmt_inserpjk->bindParam(':cust_lname', $_POST['enamn'], PDO::PARAM_STR);
-    $stmt_inserpjk->bindParam(':cust_tel', $_POST['telefon'], PDO::PARAM_STR);
-    $stmt_inserpjk->bindParam(':cust_epost', $_POST['epost'], PDO::PARAM_STR);
-    $stmt_inserpjk->bindParam(':cust_adress', $_POST['adress'], PDO::PARAM_STR);
-    $stmt_inserpjk->bindParam(':cust_postnummer', $_POST['postnummer'], PDO::PARAM_STR);
-    $stmt_inserpjk->bindParam(':cust_ort', $_POST['ort'], PDO::PARAM_STR);
+    $stmt_inserpjk->bindParam(':customer_fk', $_POST['namn'], PDO::PARAM_STR);
+    $stmt_inserpjk->bindParam(':pt_felbeskrivning', $_POST['enamn'], PDO::PARAM_STR);
+    $stmt_inserpjk->bindParam(':pt_arbetsbeskrivning', $_POST['telefon'], PDO::PARAM_STR);
+    $stmt_inserpjk->bindParam(':car_brand', $_POST['epost'], PDO::PARAM_STR);
+    $stmt_inserpjk->bindParam(':car_model', $_POST['adress'], PDO::PARAM_STR);
+    $stmt_inserpjk->bindParam(':car_reg', $_POST['postnummer'], PDO::PARAM_STR);
 
     $stmt_inserpjk->execute();
 }
